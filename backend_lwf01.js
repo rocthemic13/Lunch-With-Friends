d961e35afd50efaf7c9eb9b1c1c2495c9198cd57
unknown <mike@mike>
2019-04-09 09:30:14 -0400

    added website updates

--- backend_lwf01.js
+++ backend_lwf01.js
@@ -2,6 +2,26 @@
 $(function() {
 
 
+let createMeal = $('preparing_meal').on('submit', postMeal)
+
+    function postMeal(evnt) {
+        evnt.preventDefault();
+
+        $('posted_meal').html('')
+        let mealInfo = this.elements;
+        let index = 0;
+        while (mealInfo.item(index) !== null){
+            let confirmation = mealInfo.item(index);
+            confirmation = $(confirmation);
+            let name = confirmation.attr('name');
+            let price = confirmation.val();
+            $('#posted_meal').append('<p>' + name + price + '</p>');
+            index++;
+        }
+
+        
+    }
+
 
 
 
